7a8d7e6ae0062303583733210161d7b31c69ed49
Jose Trezza <im@jtrezza.com>
2026-02-21 11:09:50 -0500

    Possible fix

--- src/app/_components/date-formatter.tsx
+++ src/app/_components/date-formatter.tsx
@@ -1,12 +1,14 @@
 import { parseISO, format } from "date-fns";
 
 type Props = {
-  dateString: string;
+  dateString: string | Date;
 };
 
 const DateFormatter = ({ dateString }: Props) => {
-  const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, "LLLL	d, yyyy")}</time>;
+  const date = dateString instanceof Date ? dateString : parseISO(dateString);
+  const dateTime = dateString instanceof Date ? dateString.toISOString() : dateString;
+
+  return <time dateTime={dateTime}>{format(date, "LLLL d, yyyy")}</time>;
 };
 
 export default DateFormatter;
